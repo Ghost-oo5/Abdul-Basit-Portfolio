63a867dbd108516cdb2a9324e2c4edb03a9b0822
Ghost-oo5 <rajaabdulbasit005@gmail.com>
2024-05-02 00:00:02 +0500

    first commit

---
+++ utils/data/contactsData.js
@@ -0,0 +1,12 @@
+export const contactsData = {
+    email: 'rajaabdulbasit@gmail.com',
+    phone: '+923175972977',
+    address: 'V.P.O Thirpal, Tehsile and District Chakwal',
+    github: 'https://github.com/Ghost-oo5',
+    facebook: 'https://www.facebook.com/profile.php?id=100092595303195',
+    Instagram: 'https://www.instagram.com/captain_ghost005',
+    linkedIn: 'https://www.linkedin.com/in/Ghost005/',
+    twitter: 'https://twitter.com/Ghost_oo5',
+    stackOverflow: 'https://stackoverflow.com/users/15349472/captain-ghost',
+    devUsername: "GHOST 005"
+}
---
+++ utils/data/personal-data.js
@@ -0,0 +1,18 @@
+export const personalData = {
+  name: "Raja Abdul Basit",
+  profile: '/pic.jpg',
+  designation: "Software Developer",
+  description: "My name is abdul basit. I am a professional and enthusiastic programmer in my daily life. I am a quick learner with a self-learning attitude. I love to learn and explore new technologies and am passionate about problem-solving. I love almost all the stacks of web application development and love to make the web more open to the world. My core skill is based on JavaScript and I love to do most of the things using JavaScript. I am available for any kind of job opportunity that suits my skills and interests.",
+  email: 'abusaid7388@gmail.com',
+  phone: '+8801608797655',
+  address: 'Middle Badda, Dhaka, Bangladesh - 1212 ',
+  github: 'https://github.com/Ghost-oo5',
+  facebook: 'https://www.facebook.com/profile.php?id=100092595303195',
+  Instagram: 'https://www.instagram.com/captain_ghost005',
+  linkedIn: 'https://www.linkedin.com/in/Ghost005/',
+  twitter: 'https://twitter.com/Ghost_oo5',
+  stackOverflow: 'https://stackoverflow.com/users/15349472/captain-ghost',
+  devUsername: "GHOST 005",
+  leetcode: "https://leetcode.com/u/rajaabdulbasit005/",
+  resume: "https://drive.google.com/file/d/1eyutpKFFhJ9X-qpQGKhUNnVRkB5Wer00/view?usp=sharing"
+}
